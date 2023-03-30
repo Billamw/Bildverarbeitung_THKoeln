88f331648c66b59984f1d9a3c5ca782f00e559ad
Billamw <johan.dasbach@gmx.de>
2023-03-30 19:15:12 +0200

    Update Praktikum02.py

--- Johan/Praktikum02.py
+++ Johan/Praktikum02.py
@@ -1,21 +1,108 @@
 # 30.03.2023
 import cv2 as cv2
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 imgCol = cv2.imread('Utils\LenaJPEG.jpg')
 
 width, height, depth = imgCol.shape
 
 def getHistogramm(img):
-    histogram=[0]*256
+    histogram=np.zeros(256)
+    
     for y in range(height):
         for x in range(width):
-            histogram[img[y][x]] += 1
+            i = 0.2125 * img[y][x][0] + 0.7154 * img[y][x][1] + 0.072 * img[y][x][2]
+            # i = img[y][x][0]
+            histogram[int(i)] += 1
     return histogram
 
+def getNormHistogramm(img):
+    histogram=np.zeros(256)
+    
+    for y in range(height):
+        for x in range(width):
+            i = 0.2125 * img[y][x][0] + 0.7154 * img[y][x][1] + 0.072 * img[y][x][2]
+            # i = img[y][x][0]
+            histogram[int(i)] += 1
+    histogram/=(width*height)
+    return histogram
 
-plt.plot(getHistogramm(imgCol))
-cv2.imshow("img", imgCol)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def getCumulatedHistogramm(img):
+    H = np.zeros(256)
+    h = getNormHistogramm(img)
+    for i in range(len(h)):
+        for j in range(i): 
+            H[i] += h[j]
+    return H
+
+def getCumulatedHistogrammRekursiv(img):
+    H = np.zeros(256)
+    h = getNormHistogramm(img)
+    H = recursiv(H, h, 256)
+    return H
+
+def recursiv(H, h, i):
+    if i == 0:
+        H[i] = 0
+    if 0 < i < 255:
+        H[i] = recursiv(h, i-1)+h[i]
+    return H
+ 
+
+
+def linearContrast(img, t0, t1):
+    toRet = np.zeros((height, width))
+    for y in range(height):
+        for x in range(width):
+            a = 0.2125 * img[y][x][0] + 0.7154 * img[y][x][1] + 0.072 * img[y][x][2]
+            if a < t0:
+                toRet[y][x] = 0
+            if t0 <= a <= t1:
+                toRet[y][x] = 255/(t1-t0) * (a-t0)
+            if a >= t1:
+                toRet[y][x] = 255
+    return toRet
+
+def autoContrast(img):
+    toRet = np.zeros((height, width))
+    tmp = getHistogramm(img)[getPercentage(img, 0.05):getPercentage(img, 1-0.05)]
+    for y in range(height):
+        for x in range(width):
+            a = 1
+
+def getPercentage(img, schwellwert):
+    H = getCumulatedHistogramm(img)
+    for i in range(len(H)):
+        if H[i] >= schwellwert:
+            return i
+
+
+def getMedianOfHistogramm(img):
+    H = getCumulatedHistogramm(img)
+    for i in range(len(H)):
+        if H[i] >= 0.5:
+            return getHistogramm(img)[i]
+
+
+def getBinary(img, schwellwert):
+    toRet = np.zeros((height, width))
+    for y in range(height):
+        for x in range(width):
+            a = 0.2125 * img[y][x][0] + 0.7154 * img[y][x][1] + 0.072 * img[y][x][2]
+            if a < schwellwert:
+                toRet[y][x] = 0
+            else:
+                toRet[y][x] = 1
+    return toRet
+
+    
+# print(getMedianOfHistogramm(imgCol))
+
+# plt.plot(getHistogramm(imgCol))
+# plt.plot(getCumulatedHistogram(imgCol))
+plt.plot(getCumulatedHistogramm(imgCol))
+plt.show()
+# cv2.imshow("img", getBinary(imgCol, 125))
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
