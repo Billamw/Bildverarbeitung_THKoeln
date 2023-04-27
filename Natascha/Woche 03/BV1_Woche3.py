370388657c69853ad75bcd99c760bfa7f72540b0
Taschoo <NataschaWolf-1@web.de>
2023-04-27 19:10:49 +0200

    update

--- Natascha/Woche 03/BV1_Woche3.py
+++ Natascha/Woche 03/BV1_Woche3.py
@@ -1,6 +1,26 @@
 import cv2 as cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
-LennaCol = cv2.imread('Utils\LenaJPEG.jpg')
+def Binarization(image, threshhold):
+    imgBin = np.zeros((image.shape[0], image.shape[1]))
+    for y in range(height):
+        for x in range(width):
+            if 0.2125 * image[y][x][0] + 0.7154 * image[y][x][1] + 0.072 * image[y][x][2] < threshhold:
+                imgBin[y][x] = 0
+            else:
+                imgBin[y][x] = 255
+    return imgBin
 
+imgCol = cv2.imread('../../Utils/LenaJPEG.jpg')
+height, width, depth = imgCol.shape
+
+# Anzeigen des Bildes in einem Fenster
+cv2.imshow('Mein Bild', Binarization(imgCol, 100))
+
+# Warten auf eine Taste, um das Fenster zu schließen
+cv2.waitKey(0)
+
+# Schließen des Fensters
+cv2.destroyAllWindows()
