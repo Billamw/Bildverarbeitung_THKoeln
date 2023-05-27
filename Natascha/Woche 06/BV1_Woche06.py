7526790b8d21fc6dd3eb25eddec8cc66ca648bf7
Taschoo <NataschaWolf-1@web.de>
2023-05-27 18:25:08 +0200

    Update BV1_Woche06.py

--- Natascha/Woche 06/BV1_Woche06.py
+++ Natascha/Woche 06/BV1_Woche06.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 
 print("start")
 
-Path_MADN_01 = 'Utils/MenschAergere01.jpg'
-Path_MADN_02 = 'Utils/MenschAergere02.jpg'
-Path_MADN_03 = 'Utils/MenschAergere03.jpg'
+Path_MADN_01 = '../../Utils/MenschAergere01.jpg'
+Path_MADN_02 = '../../Utils/MenschAergere02.jpg'
+Path_MADN_03 = '../../Utils/MenschAergere03.jpg'
 
 MADN_01 = cv2.imread(Path_MADN_01)
 MADN_02 = cv2.imread(Path_MADN_02)
@@ -20,28 +20,29 @@ MADN_03_resize = cv2.resize(MADN_03, (int(width/5), int(height/5)))
 
 hsv_img = cv2.cvtColor(MADN_01_resize, cv2.COLOR_BGR2HSV)
 
-hsv_onlyRed = empty_array = np.zeros((height, width))
-hsv_onlyBlue = empty_array = np.zeros((height, width))
-hsv_onlyGreen = empty_array = np.zeros((height, width))
+hsv_onlyRed = np.zeros((height, width,3))
+hsv_onlyBlue = np.zeros((height, width,3))
+hsv_onlyGreen = np.zeros((height, width,3))
 
 def seperateHSV(image, mode):
-    hsv_seperated= np.zeros((image.shape[0], image.shape[1]))
-    if(mode == 1 or mode == 2): #green or blue
+    hsv_seperated = np.zeros_like(image)
+    if mode == 1 or mode == 2 : #green or blue
         if mode == 1 :
             min = 40
-            max = 70
+            max = 75
         else:
-            min = 100
+            min = 110
             max = 130
-        
-        for y in range(height):
-            for x in range(width):
-                if(min < image[y][x][0] and image[y][x][0] < max):
-                    hsv_seperated[y][x][0] = image[y][x][0]
-    
-    return hsv_seperated[y][x][0]
 
-cv2.imshow('HSV image', seperateHSV(hsv_img,1))
+    for y in range(image.shape[0]):
+            for x in range(image.shape[1]):
+                if min < image[y][x][0] < max:
+                    hsv_seperated[y][x][:] = [255,255,255]
+
+    return hsv_seperated
+
+cv2.imshow('HSV image', seperateHSV(hsv_img,2))
+#cv2.imshow('HSV image', hsv_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
